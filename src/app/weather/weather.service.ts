078371ba196d34bd576110089f7bfe8b45969f49
Anthony Muller <anthonyvmuller@outlook.com>
2025-03-10 19:28:21 -0300

    refactor: simplify weather service state

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,8 +1,6 @@
-import { HttpClient, httpResource } from '@angular/common/http';
-import { computed, effect, inject, Injectable, linkedSignal, signal } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+import { Injectable, signal, untracked } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { outputToObservable, rxResource, toObservable } from '@angular/core/rxjs-interop';
-import { map, merge, tap } from 'rxjs';
 
 interface Weather {
 	temp: number;
@@ -28,53 +26,46 @@ export class WeatherService {
   private apiUrl = environment.openWeatherApiUrl;
 
   readonly city = signal<string | null>(null);
-  readonly coordinatesCity = signal<string | null>(null);
   readonly coordinates = signal<GeolocationCoordinates | null>(null);
-  
-  readonly weatherByCity = httpResource<OpenWeatherResponse>(() => this.weatherByCityFn());
-  readonly weatherByCoordinates = httpResource<OpenWeatherResponse>(() => this.weatherByCoordinatesFn())
-  
-  readonly weather$ = merge(
-    toObservable(this.weatherByCity.value), 
-    toObservable(this.weatherByCoordinates.value)
-  ).pipe(
-    tap(value => this.coordinatesCity.set(value?.name || null)), 
-    map(value => value?.main)
-  );
 
-  readonly isLoading$ = merge(
-    toObservable(this.weatherByCity.isLoading), 
-    toObservable(this.weatherByCoordinates.isLoading)
-  )
+  readonly weather = httpResource<OpenWeatherResponse>(() => this.weatherFn());
 
   constructor() {
+    this.getByLocation();
+  }
+
+  getByLocation() {
     if (!navigator.geolocation) {
       return;
     }
 
+    untracked(() => this.city.set(null));
     navigator.geolocation.getCurrentPosition(
       (position) => this.coordinates.set(position.coords), 
       (_) => this.city.set('São Paulo'),
     );
   }
 
-  private weatherByCoordinatesFn() {
-    const coordinates = this.coordinates();
-    if (!coordinates) {
-      return;
-    }
+  private weatherFn() {
+    const params: Record<string, string | number | boolean> = {
+      appid: this.apiKey,
+      units: "metric",
+    };
 
-    const { latitude, longitude } = coordinates;
-    
-    return `${this.apiUrl}?lat=${latitude}&lon=${longitude}&appid=${this.apiKey}&units=metric`;
-  }
-
-  private weatherByCityFn() {
     const city = this.city();
-    if (!city) {
-      return;
+    const coordinates = this.coordinates();
+    if (city) {
+      params["q"] = city;
+    } else if(coordinates) {
+      params["lat"] = coordinates.latitude;
+      params["lon"] = coordinates.longitude;
+    } else {
+      return undefined;
     }
-
-    return `${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`;
+  
+    return {
+      url: `${this.apiUrl}`,
+      params,
+    };
   }
 }
